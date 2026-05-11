6cbacbdb84e65415e90b63f25c0896857c367f9f
Alexis Devillard <253889413+Alexis-Devillard@users.noreply.github.com>
2026-05-11 20:42:48 +0200

    Clean project from old tests

--- pkg/user_input/ui_hooks/startSnapshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ $# -lt 1 ]; then
-    echo "Erreur: ID du site necessaire en paramètre. Ex: startSnapshot.sh SITE1"
-    exit 1
-fi
-
-printf "ACTION:MARKER\nDEST:$1\nSENDER:USER\n\n" > /tmp/site_input_$1
--- simulations/msg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-printf "ACTION:TEST\nID:testing\nDEST:1\nSENDER:3\nCOLOR:blanc\n\n" > /tmp/backpipe
