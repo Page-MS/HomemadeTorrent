6764bfb4509eeda5217715cbc3b6bae2b641f330
boulotno <noah.boulot@etu.utc.fr>
2026-06-10 17:42:30 +0200

    Mise a jour de la file distribué (meme principe que pour lhorloge vectorielle)

--- pkg/network_controler/add_user.go
+++ pkg/network_controler/add_user.go
@@ -1,6 +1,7 @@
 package networkcontroler
 
 import (
+	"HomemadeTorrent/pkg/distributed_file"
 	"HomemadeTorrent/pkg/parser"
 	"log"
 	"sort"
@@ -11,6 +12,7 @@ import (
 func (nc *NetworkControler) AddUser(newSiteID string, isLeader bool) string {
 	oldDirectory := nc.Controler.NetworkDirectory
 	oldVector := nc.Controler.Vector.GetCopy()
+	oldTab := nc.Controler.DistFile.GetCopy()
 
 	// Création de la nouvelle liste d'IDs
 	newIDs := make([]string, len(oldDirectory.IndexToID))
@@ -34,9 +36,27 @@ func (nc *NetworkControler) AddUser(newSiteID string, isLeader bool) string {
 		newVector[newIdx] = oldVector[oldIdx]
 	}
 
+	newTab := make([]distributed_file.TabEntry, len(newIDs))
+
+	// On initialise toutes les cases par défaut (comme dans GetNewDistributedFile)
+	for i := range newTab {
+		newTab[i] = distributed_file.TabEntry{
+			Type: distributed_file.SC_LIBERATION,
+			Date: 0,
+		}
+	}
+
+	// On replace les anciennes valeurs aux bons index
+	for _, id := range oldDirectory.IndexToID {
+		oldIdx := oldDirectory.IDToIndex[id]
+		newIdx := newIDToIndex[id]
+		newTab[newIdx] = oldTab[oldIdx]
+	}
+
 	// Injection des nouvelles structures
 	newMyIndex := newIDToIndex[nc.SiteID]
 	nc.Controler.Vector.UpdateLayout(newVector, newMyIndex)
+	nc.Controler.DistFile.UpdateLayout(newTab, newMyIndex)
 
 	nc.Controler.NetworkDirectory.IndexToID = newIDs
 	nc.Controler.NetworkDirectory.IDToIndex = newIDToIndex
