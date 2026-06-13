b379f2ebcd22ff3055150886ccb2403c658beb5c
Page-MS <pagems@proton.me>
2026-06-13 22:38:54 +0200

    fin fin du merge

--- src/homemadeTorrent/main.go
+++ src/homemadeTorrent/main.go
@@ -17,18 +17,27 @@ func main() {
 			"  go run main.go Site1 /tmp/network_fifos/in_node1 2 1 Site1 Site2 Site3")
 
 	}
-
+	log.Printf("Arguments: %v\n", args)
 	siteID := args[0]
 	siteAddress := args[1]
 	nbNeighborsStr := args[2]
-	bootstrap := args[3]
+	bootstrapStr := args[3]
 	allSiteIDs := args[4:]
 
-	nbNeighbors, _ := strconv.Atoi(nbNeighborsStr)
+	bootstrap, err := strconv.Atoi(bootstrapStr)
+	if err != nil {
+		log.Fatalf("bootstrap doit être 0 ou 1 : %v", err)
+	}
+
+	nbNeighbors, err := strconv.Atoi(nbNeighborsStr)
+	if err != nil {
+		log.Fatalf("nbNeighbors doit être un entier : %v", err)
+	}
+
 	// Lancement boucle
-	if bootstrap == "1" {
+	if bootstrap == 1 {
 		event_loop.StartBootstrap(siteID)
 	} else {
-		event_loop.Start(allSiteIDs, siteID, nbNeighbors, bootstrap, siteAddress)
+		event_loop.Start(allSiteIDs, siteID, nbNeighbors, siteAddress, bootstrap)
 	}
 }
